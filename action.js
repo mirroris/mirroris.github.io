f8407fe0552ed21a7cacc4e74dcd3a512fbd62e3
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 01:00:44 +0900

    delete z-index

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 
 function handleScroll(opac) {
-  let opacity = opac - (window.scrollY / window.innerHeight);
+  let opacity = opac + (window.scrollY / window.innerHeight);
   element.style.opacity = opacity < 0 ? 0 : opacity;
   window.requestAnimationFrame(handleScroll(opacity));
 }
