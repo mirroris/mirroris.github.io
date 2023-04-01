96bd02c8b3e7c1d63141efa77d29bf03111bf0d7
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 02:43:56 +0900

    m

--- action.js
+++ action.js
@@ -1,10 +1,13 @@
 var element = document.querySelector('.mirroris');
 var windowHeight = window.innerHeight;
 var initialPostion = 0;
-var intialopacity = element.opacity;
+var intialopacity = 0.4;
+var opacity = 0.0;
+const currentColor = currentStyle.getPropertyValue('color');
 
 window.addEventListener('scroll', function() {
-  var opacity = initalopacity - 6*(distanceFromTop / window.scrollY);
+  opacity = initalopacity - 6*(window.scrollY/this.window.innerHeight);
   opacity = opacity < 0 ? 0 :opacity;
-  element.style.opacity = opacity;
+  const newColor =   currentRgba;
+  element.style.color = newColor;
 });
