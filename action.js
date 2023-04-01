bd654df716ed4cd4caf7fd58275dcea95816d4a4
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 02:12:32 +0900

    chnge positon of footer

--- action.js
+++ action.js
@@ -1,16 +1,15 @@
 var element = document.querySelector('.mirroris');
-var initialPosition = element.getBoundingClientRect().top + window.scrollY;
 var windowHeight = window.innerHeight;
+var initialPostion = 0;
 
 window.addEventListener('scroll', function() {
-  var currentPosition = element.getBoundingClientRect().top + window.scrollY;
-  var distanceFromTop = currentPosition - initialPosition;
+  var distanceFromTop =  window.scrollY - initialPosition;
 
   if (distanceFromTop < 0) {
     distanceFromTop = 0;
   }
 
-  var opacity = opacity - 6*(distanceFromTop / windowHeight);
+  var opacity = opacity - 6*(distanceFromTop / window.scrollY);
   opacity = opacity < 0 ? 0 :opacity;
   element.style.opacity = opacity;
 });
