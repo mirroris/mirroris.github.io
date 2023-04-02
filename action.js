903e3d216a9c824c557a91fa7bb8d9e24b8a552f
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 19:19:13 +0900

    solve 404 favicon

--- action.js
+++ action.js
@@ -1,13 +1,12 @@
-var element = document.querySelector('.mirroris');
+var element = document.querySelector('.mirroris::before');
 var windowHeight = window.innerHeight;
 var initialPostion = 0;
 var intialopacity = 0.4;
 var opacity = 0.0;
-const currentColor = currentStyle.getPropertyValue('color');
+const currentColor = currentStyle.getPropertyValue('opacity');
 
 window.addEventListener('scroll', function() {
   opacity = initalopacity - 6*(window.scrollY/this.window.innerHeight);
   opacity = opacity < 0 ? 0 :opacity;
-  const newColor =   currentRgba;
-  element.style.color = newColor;
+  element.style.opacity = opacity;
 });
