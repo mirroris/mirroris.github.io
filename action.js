00693bde8c5f6680ce68f9ae55bf543a25757dbc
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 23:16:56 +0900

    tuning

--- action.js
+++ action.js
@@ -1,5 +1,6 @@
 var currentMenu = "mirroris";
 function slideRoll()  {
+  console.log(this.className);
   var className = (this.className).replace("Trigger","");
   var prioritySlide = document.getElementsByClassName(className);
   var leftEdge = 1000;
