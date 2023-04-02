d03d8a02680adf749a737dc55962dc6b84f63cdd
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 23:34:56 +0900

    tuning

--- action.js
+++ action.js
@@ -1,11 +1,19 @@
 var currentMenu = "mirroris";
+var startTime = new Date();
+var currentTime;
+var prioritySlide;
+var leftEdge = 100.0;
+
 function slideRoll(className)  {
   console.log(className);
   var className = className.replace("Trigger","");
-  var prioritySlide = document.getElementsByClassName(className);
-  var leftEdge = 1000;
-  while(leftEdge>0){
-    prioritySlide.style.left = toString(leftEdge/10) + "vw";
-    leftEdge--;
+  prioritySlide = document.getElementsByClassName(className);
+  while((leftEdge>0)){
   }
 };
+
+function movement(){
+    prioritySlide.style.left = toString(leftEdge/10.0) + "vw";
+    leftEdge--;
+    setTimeout(movement,50);
+}
