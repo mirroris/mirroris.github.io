d42dc15b3643a6cbe6f4ca59cb1b0aaa8efcf6e7
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-02 23:02:42 +0900

    tuning yet

--- action.js
+++ action.js
@@ -1,20 +1,7 @@
-var element = document.querySelector('.mirroris::before');
-var windowHeight = window.innerHeight;
-var initialPostion = 0;
-var intialopacity = 0.4;
-var opacity = 0.0;
-const currentColor = currentStyle.getPropertyValue('opacity');
-
-window.addEventListener('scroll', function() {
-  opacity = initalopacity - 6*(window.scrollY/this.window.innerHeight);
-  opacity = opacity < 0 ? 0 :opacity;
-  element.style.opacity = opacity;
-});
-
-
 var currentMenu = "mirroris";
 function slideRoll()  {
-  var prioritySlide = document.getElementsByClassName(this.className.replace("Trigger",""));
+  var className = this.className.replace("Trigger","");
+  var prioritySlide = document.getElementsByClassName(className);
   var leftEdge = 1000;
   while(leftEdge>0){
     prioritySlide.style.left = toString(leftEdge/10) + "vw";
