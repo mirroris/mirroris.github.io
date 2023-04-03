e17a4c46bb3e327a35af31a9d444e627aa936c78
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-04-03 16:28:19 +0900

    renew action

--- action.js
+++ action.js
@@ -1,18 +1,23 @@
 var currentMenu = "mirroris";
-var prioritySlide;
-var leftEdge = 100.0;
+var nextSlide;
+var prevSlide;
+var leftEdge = 100;
 var prevTitle = "mirrorisTitle";
 var nextTitle;
 function slideRoll(className)  {
   console.log(className+" is pushed");
   var className = className.replace("Trigger","");
-  prioritySlide = document.getElementsByClassName(className);
+  prevSlide = nextSlide;
+  nextSlide = document.getElementsByClassName(className);
   prevTitle = nextTitle;
   nextTitle = document.getElementsByClassName(className+"Title");
   if(className != currentMenu) {
     className = currentMenu;
+    nextSlide.style.visiblity = "visible";
     movement();
     emergence();
+    prevSlide.style.visiblity = "hidden"
+    prevSlide.style.right = "100vw";
   }
   leftEdge = 100.0;
   titleOpacity=0;
@@ -21,8 +26,10 @@ function slideRoll(className)  {
 function movement(){
   if(leftEdge>0){
     let num = leftEdge;
-    prioritySlide.style.left = num.toString + "vw";
-    prevTitle.style.opacity = leftEdge/100.0;
+    nextSlide.style.left = num.toString() + "vw";
+    prevSlide.style.right = num.toString() + "vw";
+    nextTitle.style.opacity = ((100 -leftEdge)/100.0).toString;
+    prevTitle.style.opacity = (leftEdge/100.0).toString();
     leftEdge--;
     setTimeout(movement,50);
   }
