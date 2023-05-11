5823d7868ede411471cc1d59c4bd6be98dbc9d53
mirroris <u455017k@ecs.osaka-u.ac.jp>
2023-05-12 00:54:47 +0900

    impl onclick function

---
+++ js/submit.js
@@ -0,0 +1,22 @@
+function submitCode(){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://api.paiza.io/runners/create');
+  xhr.requestType = 'json';
+  var params = {
+    source_code:document.forms['code'],
+    language:document.forms['language'],
+    input:"",
+    longpoll:"",
+    longpoll_timeout:"",
+    api_key:"guest"
+  };
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    }
+    else {
+      console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();
+};
